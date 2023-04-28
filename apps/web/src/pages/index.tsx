10487db7587b6a693aacdc98a57c8dc5387cb664
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-04-29 03:55:28 +0900

    chore(sulsul): bump `@suspensive/react` version

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { css, useTheme } from '@emotion/react';
 import { Button } from '@sulsul/ui';
 import { motion } from 'framer-motion';
-import { flex, gutter } from '@jsxcss/emotion';
+import { Box, flex, gutter } from '@jsxcss/emotion';
 import { Delay, Suspense } from '@suspensive/react';
 import { useSuspenseQuery } from '@suspensive/react-query';
+import axios from 'axios';
 
 const Web = () => (
   <Suspense.CSROnly fallback={<Delay ms={4000}>hi</Delay>}>
@@ -15,20 +16,24 @@ export default Web;
 
 const LoadingComponent = () => {
   const theme = useTheme();
-  const query = useSuspenseQuery<{ userId: string }>([], async () => {
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve('hi');
-      }, 3000);
-    });
+  const query = useSuspenseQuery({
+    queryKey: [],
+    queryFn: async () => {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve('hi');
+        }, 3000);
+      });
 
-    return fetch('https://jsonplaceholder.typicode.com/posts/1').then((response) => response.json());
+      return axios.get<{ userId: string }>('https://jsonplaceholder.typicode.com/posts/1').then(({ data }) => data);
+    },
   });
 
   return (
     <>
       {query.data.userId}
-      <motion.div
+      <Box
+        as={motion.div}
         whileTap={{ scale: 1.2 }}
         css={css`
           background-color: ${theme.colors.black};
@@ -41,7 +46,22 @@ const LoadingComponent = () => {
         <Button />
         <Button />
         <Button />
-      </motion.div>
+      </Box>
+      <Box
+        as={motion.div}
+        whileTap={{ scale: 1.2 }}
+        css={css`
+          background-color: ${theme.colors.black};
+          ${flex({ direction: 'column' })}
+          ${gutter({ direction: 'vertical', spacing: 24 })}
+        `}
+      >
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </Box>
     </>
   );
 };
