eb411797fc91046ce351c3098a0ae938f992fb3b
huewilliams <hswspwgod@gmail.com>
2023-06-19 21:54:24 +0900

    chore(web-view): apply Image component

--- apps/explore/src/app/page.tsx
+++ apps/explore/src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client';
+
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
-      <Image src={"bubble_wine.svg"} alt={"bubble_wine"}/>
+      <Image src={"bubble_wine.svg"} alt={"bubble_wine"} width={92} height={92}/>
     </main>
   );
 }
