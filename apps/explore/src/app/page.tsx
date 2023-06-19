d9d9c4e16d74cf8c65a12563fdc23be0818802e7
deli-ght <minjju0660@gmail.com>
2023-06-19 22:09:05 +0900

    fix(web-view): fix lint

--- apps/explore/src/app/page.tsx
+++ apps/explore/src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-      <Image src={'bubble_wine.svg'} alt={'bubble_wine'} width={92} height={92} />
+      <Image src="bubble_wine.svg" alt="bubble_wine" width={92} height={92} />
     </main>
   );
 }
