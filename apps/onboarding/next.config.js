d42b1f04115f353431c095bbe41b3293e8c01e68
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-06-25 20:53:28 +0900

    fix(onboarding): add packages as transpilePackages

--- apps/onboarding/next.config.js
+++ apps/onboarding/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@sulsul/ui', '@sulsul/token'],
+  transpilePackages: [
+    '@sulsul/ui',
+    '@sulsul/token',
+    'react-hook-step',
+    '@suspensive/react',
+    '@suspensive/react-query',
+  ],
   compiler: {
     emotion: true,
   },
