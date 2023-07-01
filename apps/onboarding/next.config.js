274c9ccd014d6e1cb7daeb25679385a0b5b0d5df
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-07-01 15:53:51 +0900

    feat(apps/onboarding): setup svgr (#17)
    # apps/onboarding에 svgr을 설정합니다
    
    localhost:3000/tests/svgr 에서 확인해주세요
    next 프로젝트에서 svg를 쉽게 사용할 수 있게 설정해요
    
    ---------
    
    Co-authored-by: Minju Kim <minjju0660@gmail.com>

--- apps/onboarding/next.config.js
+++ apps/onboarding/next.config.js
@@ -8,6 +8,13 @@ const nextConfig = {
     '@suspensive/react',
     '@suspensive/react-query',
   ],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
   compiler: {
     emotion: true,
   },
