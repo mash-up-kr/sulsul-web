a35ec6b6ded620d9699c6b6b7c19e8cb41c4c8ef
deli-ght <minjju0660@gmail.com>
2023-07-09 23:00:17 +0900

    chore(css): add emotion babel for css props

--- apps/onboarding/.eslintrc.js
+++ apps/onboarding/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
   extends: ['@sulsul/eslint-config/react-ts-noimport', 'plugin:@next/next/recommended'],
+  rules: {
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+  },
 };
--- apps/onboarding/next.config.js
+++ apps/onboarding/next.config.js
@@ -21,13 +21,6 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
