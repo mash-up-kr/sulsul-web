64ee33bc99212ffcd05d9b732416205857a400a0
deli-ght <minjju0660@gmail.com>
2023-07-22 13:08:39 +0900

    fix(Button): fix use Button as lib

--- apps/onboarding/src/app/page.tsx
+++ apps/onboarding/src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from '@sulsul/ui';
+import { Button } from './components/Button';
 
 export default function Home() {
   return (
--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,10 +1,7 @@
-'use client';
-
-/** @jsxImportSource @emotion/react */
-
 import { ComponentPropsWithoutRef, forwardRef } from 'react';
-// import { css } from '@emotion/react';
-// import { token } from '@sulsul/token';
+import { css } from '@emotion/react';
+import { token } from '@sulsul/token';
+import '../global.css';
 
 type ButtonProps = ComponentPropsWithoutRef<'button'> & {
   appearance?: 'primary' | 'default' | 'minimal';
@@ -12,82 +9,86 @@ type ButtonProps = ComponentPropsWithoutRef<'button'> & {
   isLoading?: boolean;
 };
 
-// const contentsStyle = css`
-//   border-radius: 16px;
-//   cursor: pointer;
+const contentsStyle = css`
+  border-radius: 16px;
+  cursor: pointer;
 
-//   &:disabled {
-//     opacity: 0.32;
-//   }
-// `;
+  &:disabled {
+    opacity: 0.32;
+  }
+`;
 
-// // button appearance styles
-// const primaryStyle = css`
-//   background-color: ${token.colors.primary[100]};
-//   color: ${token.colors.white};
+// button appearance styles
+const primaryStyle = css`
+  background-color: ${token.colors.primary[100]};
+  color: ${token.colors.white};
 
-//   &:hover {
-//     background-color: ${token.colors.primary[200]};
-//   }
-// `;
+  &:hover {
+    background-color: ${token.colors.primary[200]};
+  }
+`;
 
-// const defaultStyle = css`
-//   background-color: ${token.colors.grey[300]};
-//   color: ${token.colors.white};
+const defaultStyle = css`
+  background-color: ${token.colors.grey[300]};
+  color: ${token.colors.white};
 
-//   &:hover {
-//     background-color: ${token.colors.grey[200]};
-//   }
-// `;
+  &:hover {
+    background-color: ${token.colors.grey[200]};
+  }
+`;
 
-// const minimalStyle = css`
-//   background-color: transparent;
-//   color: ${token.colors.grey[700]};
+const minimalStyle = css`
+  background-color: transparent;
+  color: ${token.colors.grey[700]};
 
-//   &:hover {
-//     background-color: ${token.colors.primary[200]};
-//     opacity: 0.8;
-//     color: ${token.colors.primary[100]};
-//   }
-// `;
+  &:hover {
+    background-color: ${token.colors.primary[200]};
+    opacity: 0.8;
+    color: ${token.colors.primary[100]};
+  }
+`;
 
-// // button size styles
-// const lgStyle = css`
-//   ${token.text.subtitle[2]}
-//   padding : 17px 16px;
-// `;
+// button size styles
+const lgStyle = css`
+  ${token.text.subtitle[2]}
+  padding : 17px 16px;
+`;
 
-// const mdStyle = css`
-//   ${token.text.subtitle[3]}
-//   padding : 9px 12px;
-// `;
+const mdStyle = css`
+  ${token.text.subtitle[3]}
+  padding : 9px 12px;
+`;
 
-// const smStyle = css`
-//   ${token.text.subtitle[4]}
-//   padding : 5px 8px;
-// `;
+const smStyle = css`
+  ${token.text.subtitle[4]}
+  padding : 5px 8px;
+`;
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(
   props,
   ref
 ) {
   const { children, appearance = 'default', size = 'md', ...restProps } = props;
-  console.log(appearance, size);
 
-  // const appearanceStyle = {
-  //   primary: primaryStyle,
-  //   default: defaultStyle,
-  //   minimal: minimalStyle,
-  // }[appearance];
+  const appearanceStyle = {
+    primary: primaryStyle,
+    default: defaultStyle,
+    minimal: minimalStyle,
+  }[appearance];
 
-  // const sizeStyle = {
-  //   lg: lgStyle,
-  //   md: mdStyle,
-  //   sm: smStyle,
-  // }[size];
+  const sizeStyle = {
+    lg: lgStyle,
+    md: mdStyle,
+    sm: smStyle,
+  }[size];
 
   return (
-    <button ref={ref} type="button" {...restProps}>
+    <button
+      ref={ref}
+      css={[contentsStyle, appearanceStyle, sizeStyle]}
+      type="button"
+      {...restProps}
+    >
       {children}
     </button>
   );
