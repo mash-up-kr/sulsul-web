45e5736caaad0474ea2b5df2694d6fa860f4ef9f
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-07-23 22:19:37 +0900

    fix(onboarding): update measure style

--- apps/onboarding/src/app/measure/page.tsx
+++ apps/onboarding/src/app/measure/page.tsx
@@ -1,10 +1,12 @@
+/** @jsxImportSource @emotion/react */
+
 'use client';
 import { css } from '@emotion/react';
 import { Stack, Box, Flex } from '@jsxcss/emotion';
 import { token } from '@sulsul/token';
 import { Button } from '@sulsul/ui';
 import { useRouter } from 'next/navigation';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { DrinkResDrinkTypeEnum } from '~/api';
@@ -12,8 +14,7 @@ import IcBack from '~/assets/icons/navigations/ic-back.svg';
 import IcDoublechevronLeft from '~/assets/icons/ic_doublechevron_left.svg';
 import IcDoublechevronRight from '~/assets/icons/ic_doublechevron_right.svg';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useState } from 'react';
-import { wrap } from 'popmotion';
+import { useRef } from 'react';
 // import IcRefresh from '~/assets/icons/navigations/ic-refresh.svg';
 
 const drinkTypes = [
@@ -33,36 +34,21 @@ type MeasurePageFormValues = z.infer<typeof measurePageSchema>;
 
 export default function MeasurePage() {
   const router = useRouter();
+  const constraintsRef = useRef<HTMLDivElement>(null);
   const { setValue, watch } = useForm<MeasurePageFormValues>({
     defaultValues: {
-      drinkType: '소주',
+      drinkType: '고량주',
       drinks: [],
     },
+    mode: 'all',
     resolver: zodResolver(measurePageSchema),
   });
+
   const [drinkType, drinks] = watch(['drinkType', 'drinks']);
-
-  const [visible, setVisible] = useState(0);
-  const [back, setBack] = useState(false);
-  //박스마다 이미지 적용
-  const imageIndex = wrap(0, drinkTypes.length, visible);
-
-  const nextPlease = () => {
-    setBack(false);
-    setVisible((prev) =>
-      prev === drinkTypes.length - 1 ? drinkTypes.length - 1 : prev + 1
-    );
-  };
-  const prevPlease = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 0 ? 0 : prev - 1));
-  };
+  const activeDrinkIndex = drinkTypes.findIndex((item) => item === drinkType);
 
   const addDrink = () => setValue('drinks', [...drinks, drinkType]);
   const removeDrink = () => setValue('drinks', drinks.slice(0, -1));
-
-  const color1 = '#F97C7280';
-  const color2 = '#4C94FF80';
 
   return (
     <Stack.Vertical
@@ -78,62 +64,7 @@ export default function MeasurePage() {
         background-size: contain;
       `}
     >
-      <Box
-        as="section"
-        css={css`
-          display: flex;
-          flex-direction: column;
-          position: relative;
-          max-width: 700px;
-          margin: 0 auto;
-          height: 100%;
-
-          &::before {
-            content: '';
-            position: absolute;
-            top: 150px;
-            left: 0;
-            background-color: ${color1};
-            border-radius: 50%;
-            box-shadow: 0px 0px 150px 150px ${color1};
-            animation: move1 5s ease-in-out infinite;
-          }
-
-          &::after {
-            content: '';
-            position: absolute;
-            bottom: 150px;
-            right: 0;
-            background-color: ${color2};
-            border-radius: 50%;
-            box-shadow: 0px 0px 150px 150px ${color2};
-            animation: move2 5s ease-in-out infinite;
-          }
-
-          @keyframes move1 {
-            0% {
-              transform: translate(0, 0) scale(1);
-            }
-            50% {
-              transform: translate(80px, 150px) scale(1.2) rotate(45deg);
-            }
-            100% {
-              transform: translate(0, 0) scale(1);
-            }
-          }
-          @keyframes move2 {
-            0% {
-              transform: translate(0, 0) scale(1);
-            }
-            50% {
-              transform: translate(-150px, -50px) scale(1.2) rotate(45deg);
-            }
-            100% {
-              transform: translate(0, 0) scale(1);
-            }
-          }
-        `}
-      >
+      <Box as="section" css={sectionCss}>
         <Stack.Horizontal justify="space-between" align="center" padding="8px 16px">
           <IcBack onClick={() => router.push('/')} />
           <div>{/* <IcRefresh /> */}</div>
@@ -157,52 +88,90 @@ export default function MeasurePage() {
           >
             <div>평소에 즐겨마시는 술로</div>
             <div>주량을 알려주세요</div>
+            <div>{drinkType}</div>
           </Stack.Vertical>
         </Stack.Vertical>
-
-        <Flex.Center width={200} margin="auto">
-          <Flex.CenterHorizontal
-            position="relative"
-            width={200}
+        <Flex direction="row" width={400 + 40 + 40} margin="auto">
+          <Box
+            as={motion.button}
             height={40}
-            as={motion.div}
+            width={40}
+            onClick={() => {
+              const nextDrinkType = drinkTypes[activeDrinkIndex - 1];
+
+              if (nextDrinkType) {
+                setValue('drinkType', nextDrinkType);
+              }
+            }}
           >
-            <AnimatePresence custom={back}>
-              <Stack.Horizontal
-                align="center"
-                as={motion.div}
-                custom={back}
-                variants={boxVariants}
-                initial="entry"
-                animate="center"
-                exit="exit"
-                key={visible}
-                css={{
-                  ...token.text.heading[1],
-                }}
-              >
-                {drinkTypes[imageIndex]}
-              </Stack.Horizontal>
-            </AnimatePresence>
-            <Flex
-              position="absolute"
-              left={0}
-              right={0}
-              top={0}
-              bottom={0}
-              justify="space-between"
+            <IcDoublechevronLeft />
+          </Box>
+          <Flex.Center
+            position="relative"
+            overflow="hidden"
+            ref={constraintsRef}
+            width={400}
+            height={40}
+            margin="auto"
+          >
+            <Stack.Horizontal
+              as={motion.div}
+              onDragEnd={(_, info) => {
+                const offset = info.offset.x;
+                if (Math.abs(offset) > 20) {
+                  const direction = offset < 0 ? 1 : -1;
+
+                  const nextDrinkType = drinkTypes[activeDrinkIndex + direction];
+                  if (nextDrinkType) {
+                    setValue('drinkType', nextDrinkType);
+                  }
+                }
+              }}
+              dragConstraints={constraintsRef}
+              drag="x"
+              css={css`
+                height: 40px;
+                display: flex;
+                flex-direction: row;
+              `}
+              animate={{
+                x:
+                  -1 *
+                    activeDrinkIndex *
+                    ((constraintsRef.current?.offsetWidth || 400) / 2) +
+                  (constraintsRef.current?.offsetWidth || 400),
+              }}
             >
-              <Box as={motion.button} height={40} width={40} onClick={prevPlease}>
-                <IcDoublechevronLeft />
-              </Box>
-              <Box as={motion.button} height={40} width={40} onClick={nextPlease}>
-                <IcDoublechevronRight />
-              </Box>
-            </Flex>
-          </Flex.CenterHorizontal>
-        </Flex.Center>
+              {drinkTypes.map((drinkType) => (
+                <Flex.Center
+                  key={drinkType}
+                  width={(constraintsRef.current?.offsetWidth ?? 400) / 2}
+                  height={40}
+                  {...token.text.heading[1]}
+                >
+                  {drinkType}
+                </Flex.Center>
+              ))}
+            </Stack.Horizontal>
+          </Flex.Center>
+          <Box
+            as={motion.button}
+            height={40}
+            width={40}
+            onClick={() => {
+              const nextDrinkType = drinkTypes[activeDrinkIndex + 1];
+              if (nextDrinkType) {
+                console.log(nextDrinkType);
+                setValue('drinkType', nextDrinkType);
+              }
+            }}
+          >
+            <IcDoublechevronRight />
+          </Box>
+        </Flex>
 
         <Box flex={1}>웹뷰</Box>
+        {drinks}
 
         <Stack.Vertical spacing={16} padding="0 16px 40px 16px">
           <Flex.Center>
@@ -238,20 +207,57 @@ export default function MeasurePage() {
   );
 }
 
-// animation
-const boxVariants = {
-  entry: (back: boolean) => ({
-    x: back ? -100 : 100,
-    opacity: 0,
-  }),
-  center: {
-    opacity: 1,
-    x: 0,
-    transition: { duration: 0.5 },
-  },
-  exit: (back: boolean) => ({
-    x: back ? 100 : -100,
-    opacity: 0,
-    transition: { duration: 0.5 },
-  }),
-};
+const sectionCss = css`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  max-width: 700px;
+  width: 100vw;
+  margin: 0 auto;
+  height: 100%;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 150px;
+    left: 0;
+    background-color: #f97c7280;
+    border-radius: 50%;
+    box-shadow: 0px 0px 150px 150px #f97c7280;
+    animation: move1 5s ease-in-out infinite;
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 150px;
+    right: 0;
+    background-color: #4c94ff80;
+    border-radius: 50%;
+    box-shadow: 0px 0px 150px 150px #4c94ff80;
+    animation: move2 5s ease-in-out infinite;
+  }
+
+  @keyframes move1 {
+    0% {
+      transform: translate(0, 0) scale(1);
+    }
+    50% {
+      transform: translate(80px, 150px) scale(1.2) rotate(45deg);
+    }
+    100% {
+      transform: translate(0, 0) scale(1);
+    }
+  }
+  @keyframes move2 {
+    0% {
+      transform: translate(0, 0) scale(1);
+    }
+    50% {
+      transform: translate(-150px, -50px) scale(1.2) rotate(45deg);
+    }
+    100% {
+      transform: translate(0, 0) scale(1);
+    }
+  }
+`;
