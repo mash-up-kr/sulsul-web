b526b5510c850b0adc37126f52965947bc139d8a
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-07-26 02:14:56 +0900

    feat: guarantee webview

--- apps/falling/src/app/layout.tsx
+++ apps/falling/src/app/layout.tsx
@@ -7,6 +7,7 @@ import { SuspensiveConfigs, SuspensiveProvider } from '@suspensive/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Inter } from 'next/font/google';
+import { AndroidWebviewProvider } from '~/contexts/androidWeview';
 import RootStyleRegistry from '../../emotion';
 
 import './globals.css';
@@ -40,16 +41,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <RootStyleRegistry>
-          <MediaQueryProvider pxs={[0, 768, 1440]}>
-            <SuspensiveProvider configs={suspensiveConfigs}>
-              <QueryClientProvider client={queryClient}>
-                <ThemeProvider theme={token}>{children}</ThemeProvider>
-                <ReactQueryDevtools initialIsOpen={false} />
-              </QueryClientProvider>
-            </SuspensiveProvider>
-          </MediaQueryProvider>
-        </RootStyleRegistry>
+        <AndroidWebviewProvider>
+          <RootStyleRegistry>
+            <MediaQueryProvider pxs={[0, 768, 1440]}>
+              <SuspensiveProvider configs={suspensiveConfigs}>
+                <QueryClientProvider client={queryClient}>
+                  <ThemeProvider theme={token}>{children}</ThemeProvider>
+                  <ReactQueryDevtools initialIsOpen={false} />
+                </QueryClientProvider>
+              </SuspensiveProvider>
+            </MediaQueryProvider>
+          </RootStyleRegistry>
+        </AndroidWebviewProvider>
       </body>
     </html>
   );
