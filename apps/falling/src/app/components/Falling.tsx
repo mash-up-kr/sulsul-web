5e08d7fd54a578433acdc799f32e889f21e90628
deli-ght <minjju0660@gmail.com>
2023-07-25 23:23:59 +0900

    feat(assets): add assets for test

--- apps/falling/src/app/components/Falling.tsx
+++ apps/falling/src/app/components/Falling.tsx
@@ -10,6 +10,17 @@ export const Falling = () => {
     }
   }, []);
 
-  const { boxRef, canvasRef } = useStackBall();
-  return <StackView boxRef={boxRef} canvasRef={canvasRef} />;
+  const { addBall, boxRef, canvasRef } = useStackBall();
+
+  const addDrink = () => {
+    addBall('/images/bubble-beer.png', 100);
+  };
+
+  return (
+    <div style={{ width: '100%', height: '600px' }}>
+      <h1>HI</h1>
+      <button onClick={addDrink}>Add Drink</button>
+      <StackView boxRef={boxRef} canvasRef={canvasRef} />
+    </div>
+  );
 };
