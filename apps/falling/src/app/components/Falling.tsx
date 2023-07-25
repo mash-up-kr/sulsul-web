5d348f7f310042568eda05172bc1bd6b336ffc0a
deli-ght <minjju0660@gmail.com>
2023-07-26 00:48:41 +0900

    feat(falling): add add event

--- apps/falling/src/app/components/Falling.tsx
+++ apps/falling/src/app/components/Falling.tsx
@@ -1,15 +1,12 @@
 'use client';
 
 import { StackView, useStackBall } from '@sulsul/ui';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 export const Falling = () => {
-  const [counts, setCounts] = useState(0);
-  const [messageFromAndroid, setMessageFromAndroid] = useState('');
-
   useEffect(() => {
-    const eventFromAndroid = async (event: any) => {
-      setMessageFromAndroid(event.detail.data);
+    const eventFromAndroid = async () => {
+      addBall('/images/bubble-beer.png', 100);
     };
 
     window.addEventListener('javascriptFunction', eventFromAndroid);
@@ -25,17 +22,8 @@ export const Falling = () => {
 
   const { addBall, boxRef, canvasRef } = useStackBall();
 
-  const addDrink = () => {
-    addBall('/images/bubble-beer.png', 100);
-    setCounts((prev) => prev + 1);
-  };
-
   return (
-    <div style={{ width: '100%', height: '600px', color: '#f542b6' }}>
-      <h1>HI</h1>
-      <h2>{counts}</h2>
-      <h2>message : {messageFromAndroid}</h2>
-      <button onClick={addDrink}>Add Drink</button>
+    <div style={{ width: '100%', height: '100vh' }}>
       <StackView boxRef={boxRef} canvasRef={canvasRef} />
     </div>
   );
