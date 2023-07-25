08ba5cf47a9783f434e47c38382a5817a79d4aef
deli-ght <minjju0660@gmail.com>
2023-07-25 23:36:34 +0900

    feat(falling): add count for test

--- apps/falling/src/app/components/Falling.tsx
+++ apps/falling/src/app/components/Falling.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { StackView, useStackBall } from '@sulsul/ui';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Falling = () => {
+  const [counts, setCounts] = useState(0);
+
   useEffect(() => {
     if (window.android) {
       window.android.showToastMessage('Hello Native Callback');
@@ -14,11 +16,13 @@ export const Falling = () => {
 
   const addDrink = () => {
     addBall('/images/bubble-beer.png', 100);
+    setCounts((prev) => prev + 1);
   };
 
   return (
-    <div style={{ width: '100%', height: '600px' }}>
+    <div style={{ width: '100%', height: '600px', color: '#f542b6' }}>
       <h1>HI</h1>
+      <h2>{counts}</h2>
       <button onClick={addDrink}>Add Drink</button>
       <StackView boxRef={boxRef} canvasRef={canvasRef} />
     </div>
