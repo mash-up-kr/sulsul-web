7b1253b850a1250efe1f1be8403bfe79b0344081
deli-ght <minjju0660@gmail.com>
2023-07-26 00:18:54 +0900

    feat(falling): add event

--- apps/falling/src/app/components/Falling.tsx
+++ apps/falling/src/app/components/Falling.tsx
@@ -5,11 +5,22 @@ import { useEffect, useState } from 'react';
 
 export const Falling = () => {
   const [counts, setCounts] = useState(0);
+  const [messageFromAndroid, setMessageFromAndroid] = useState('');
 
   useEffect(() => {
+    const eventFromAndroid = async (event: any) => {
+      setMessageFromAndroid(event.detail.data);
+    };
+
+    window.addEventListener('javascriptFunction', eventFromAndroid);
+
     if (window.android) {
-      window.android.showToastMessage('Hello Native Callback');
+      window.android.callJavaScriptFunction();
     }
+
+    return () => {
+      window.removeEventListener('javascriptFunction', eventFromAndroid);
+    };
   }, []);
 
   const { addBall, boxRef, canvasRef } = useStackBall();
@@ -23,6 +34,7 @@ export const Falling = () => {
     <div style={{ width: '100%', height: '600px', color: '#f542b6' }}>
       <h1>HI</h1>
       <h2>{counts}</h2>
+      <h2>{messageFromAndroid}</h2>
       <button onClick={addDrink}>Add Drink</button>
       <StackView boxRef={boxRef} canvasRef={canvasRef} />
     </div>
