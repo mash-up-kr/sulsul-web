4e4013d83c4504be450ebeba6d3232ff1c265df7
dana <dana@vessl.ai>
2023-07-27 14:04:38 +0900

    fix(webview): check webview is ready

--- apps/falling/src/app/components/Falling.tsx
+++ apps/falling/src/app/components/Falling.tsx
@@ -5,17 +5,24 @@ import { useEffect } from 'react';
 
 export const Falling = () => {
   const { addBall, boxRef, canvasRef } = useStackBall();
+
   useEffect(() => {
-    const addItem = async () => {
+    const addItem = () => {
       addBall('/images/bubble-beer.png', 100);
     };
 
-    window.addEventListener('addBall', addItem);
+    if (window.sulsulBridge && addBall) {
+      window.addEventListener('addBall', addItem);
+    }
 
     return () => {
       window.removeEventListener('addBall', addItem);
     };
   }, [addBall]);
+
+  if (!window.sulsulBridge) {
+    return <div>loading...</div>;
+  }
 
   return (
     <div style={{ width: '100%', height: '100vh' }}>
