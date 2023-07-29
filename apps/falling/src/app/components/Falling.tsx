b5ee4ffd57d906946590635276ca39983b345271
deli-ght <minjju0660@gmail.com>
2023-07-29 16:23:53 +0900

    feat(falling): check first event

--- apps/falling/src/app/components/Falling.tsx
+++ apps/falling/src/app/components/Falling.tsx
@@ -1,14 +1,17 @@
 'use client';
 
 import { StackView, useStackBall } from '@sulsul/ui';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { drinkImage, drinks } from '../constants/drinks';
 
 export const Falling = () => {
+  const [isFirstTouch, setIsFirstTouch] = useState(false);
   const { addBall, boxRef, canvasRef } = useStackBall();
 
   useEffect(() => {
     const addItem = (event: any) => {
+      if (!isFirstTouch) return;
+
       if (event.detail.data === undefined) {
         const { image, size } = drinkImage['소주'];
         addBall(image, size);
@@ -25,7 +28,10 @@ export const Falling = () => {
   }, [addBall]);
 
   return (
-    <div style={{ width: '100%', height: '100dvh' }}>
+    <div
+      style={{ width: '100%', height: '100dvh' }}
+      onClick={() => setIsFirstTouch(true)}
+    >
       <StackView boxRef={boxRef} canvasRef={canvasRef} />
     </div>
   );
