ae85606eb231d52ad7417df58fbbe1dddfa1ebd1
Minju Kim <minjju0660@gmail.com>
2023-07-29 16:06:16 +0900

    Feature/check android (#32)
    # Overview

---
+++ packages/ui/src/StackView/TouchButton.tsx
@@ -0,0 +1,15 @@
+export const TouchButton = () => {
+  return (
+    <svg
+      width="62"
+      height="62"
+      viewBox="0 0 62 62"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle opacity="0.4" cx="31" cy="31" r="22.5" fill="white" stroke="white" />
+      <circle opacity="0.7" cx="31" cy="31" r="30.5" stroke="white" />
+      <circle opacity="0.8" cx="31" cy="31" r="14" fill="white" fillOpacity="0.9" />
+    </svg>
+  );
+};
