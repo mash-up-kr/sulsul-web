d270a68bf9672473ad9df0dcc686db0203e8b949
deli-ght <minjju0660@gmail.com>
2023-08-07 21:53:56 +0900

    fix(lint): fix lint

--- apps/onboarding/src/app/result/components/ResultContents.tsx
+++ apps/onboarding/src/app/result/components/ResultContents.tsx
@@ -77,7 +77,6 @@ export const ResultContents = () => {
   const glasses = Number(searchParams?.get('glasses'));
   const baseY = useMotionValue(0);
   const backgroundRef = useRef<HTMLDivElement>(null);
-  
   const scrollHeight = backgroundRef.current?.scrollHeight ?? 0;
   const opacityOfY = useTransform(
     baseY,
